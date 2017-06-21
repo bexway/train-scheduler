23b31415ab092f9204e670d93c4a01caea582556
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-20 20:56:28 -0500

    master - delete button functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,21 +2,26 @@ firebase.initializeApp(config);
 
 var trainID = 0;
 var database = firebase.database();
+var lastDeleted = -1;
 
 //https://stackoverflow.com/questions/23449917/run-js-function-every-new-minute
-var time = new Date(),
-    secondsRemaining = (60 - time.getSeconds()) * 1000;
 
+//set the initial time and update it each second
+$('#clock').text(moment().format("hh:mm:ss a"));
+setInterval(function(){
+  $('#clock').text(moment().format("hh:mm:ss a"));
+}, 1000);
+
+//calculate how many minutes until the next full minute
+var time = new Date();
+var secondsRemaining = (60 - time.getSeconds()) * 1000 - time.getMilliseconds();
+//use a timeout to start the interval at the next full minute
 setTimeout(function() {
-    setInterval(function(){
-      $('.trainRow').each(function(){
-        var nextArrival = $(this).children(".nextArrival").text();
-        var arrivalObj = calcNextArrival($(this).data("data-firstTime"), $(this).data("data-frequency"));
-        $(this).children(".nextArrival").text(arrivalObj.nextArrival);
-        $(this).children(".minutesToNext").text(arrivalObj.minutesToNext);
-
-      });
-    }, 60000);
+  //run initially, then again every minute
+  updateArrivalTimes();
+  setInterval(function(){
+    updateArrivalTimes();
+  }, 60000);
 }, secondsRemaining);
 
 
@@ -57,7 +62,7 @@ database.ref().on("child_added", function(snapshot) {
   //creating an object with the next train's arrival time and the minutes to the next train
   var arrivalObj = calcNextArrival(obj.firstTime, obj.frequency);
   //creates a row with a bunch of columns
-  var row = $('<tr class="trainRow" id="train'+trainID+'">').data("data-key", key).append($('<td>').html(obj.name)).append($('<td>').html(obj.destination)).append($('<td class="nextArrival">').html(arrivalObj.nextArrival)).append($('<td class="minutesToNext">').html(arrivalObj.minutesToNext));
+  var row = $('<tr class="trainRow" id="train'+trainID+'">').data("data-key", key).append($('<td class="trainName">').html(obj.name)).append($('<td class="trainDestination">').html(obj.destination)).append($('<td class="nextArrival">').html(arrivalObj.nextArrival)).append($('<td class="minutesToNext">').html(arrivalObj.minutesToNext));
   //creating a button with the key for the db data attached, and the ID to find this row
   var button = $("<button>").html("x").addClass("deleteButton").data("data-key", key).attr("data-trainID", trainID);
   //add the button, firsttime and frequency to the row (for deleting and updating each minute)
@@ -71,13 +76,41 @@ database.ref().on("child_added", function(snapshot) {
   //use local storage for offline
 });
 
+$(document).on('click', '.deleteButton', function(){
+  console.log(this);
+  database.ref().child($(this).data("data-key")).remove();
+  lastDeleted = $(this).attr("data-trainID");
+
+});
+
+database.ref().on("child_removed", function(snapshot){
+  console.log(snapshot.val());
+  $('.trainRow').each(function(){
+    var button = $(this).find(".deleteButton");
+    if(button.data("data-key")===snapshot.key){
+      $(this).remove();
+    }
+  });
+}, function(errorObject){
+  console.log("Errors handled: "+errorObject.code);
+});
+//
+// $(document).on('click', '.editButton', function(){
+//check state. if editing, submit. if not editing, start edit
+// });
 
 
 
 
 
-
-
+function updateArrivalTimes(){
+  $('.trainRow').each(function(){
+    var nextArrival = $(this).children(".nextArrival").text();
+    var arrivalObj = calcNextArrival($(this).data("data-firstTime"), $(this).data("data-frequency"));
+    $(this).children(".nextArrival").text(arrivalObj.nextArrival);
+    $(this).children(".minutesToNext").text(arrivalObj.minutesToNext);
+  });
+}
 
 function calcNextArrival(firstTime, frequency){
   var firstTimeMoment = moment(firstTime, "HH:mm").subtract(1,"years");
@@ -98,7 +131,6 @@ function isValidTime(hours, minutes){
     return false;
   }
   else{
-    console.log(true);
     return true;
   }
 }
